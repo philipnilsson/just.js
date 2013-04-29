51c136ecdc5721efa342979a2d178c6baa2c645e
phni <phni@lundbeck.com>
2013-04-29 20:27:50 +0200

    Adding contexts

--- templ.js
+++ templ.js
@@ -10,61 +10,70 @@ function mconcat(tmpls) {
     return tmpls.reduce(append, mnull())
 }
 function tell(s) {
-    return new tmpl(function(r) {
+    return new tmpl(function(_) { return function(r) {
       return { value: null, template: s } 
-    })
+    }})
 }
 function ask(by) {
-    return new tmpl(function(r) {
-        var value
-        if (by !== null)
-            value = interpolate(by).run(r).value
-        else 
-            value = r
-        return { value: value, template: mnull() }
+    return new tmpl(function(c) { 
+        var f = function (r) { return r }
+        if (by !== null) 
+            f = function(r) { return interpolate(by).run(c)(r).value }
+        return function(r) {
+            return { value: f(r), template: mnull() }
+        }
     })
 }
 function value(x) {
-    return new tmpl(function(x) {
+    return new tmpl(function(_) { return function(x) {
         return { value: x, template: mnull() }
-    })
+    }})
 }
 function tmpl(run){ 
     this.run = run
+    this.compile = run
     this.flatMap = function(f) {
         var self = this
-        return new tmpl(function(r) {
-            var resA = self.run(r)
-            var resB = f(resA.value).run(r)
-            return { 
-                value: resB.value, 
-                template: append(resA.template, resB.template)
+        return new tmpl(function(c) { 
+            var runA = self.run(c)
+            return function(r) {
+                var resA = runA(r)
+                var resB = f(resA.value).run(c)(r)
+                return { 
+                    value: resB.value, 
+                    template: append(resA.template, resB.template)
             }
-        })
+        }})
     }
     this.repeat = function() {
         var self = this
-        return new tmpl(function(arr) {
-            var tmpl = [], vals = []
-            for (var i in arr) {
-                var res = self.run(arr[i])
-                vals.push(res.value)
-                tmpl.push(res.template)
-            }
-            return { 
-                value: vals,
-                template: mconcat(tmpl)
-            }
-        })
+        return new tmpl(function(c){ 
+            var run = self.run(c)
+            return function(arr) {
+                var tmpl = [], vals = []
+                for (var i in arr) {
+                    var res = run(arr[i])
+                    vals.push(res.value)
+                    tmpl.push(res.template)
+                }
+                return { 
+                    value: vals,
+                    template: mconcat(tmpl)
+                }
+            }})
     }
     this.then = function(template, f) {
         var self = this
         if (f == null) f = function(x) { return x }
-        return new tmpl(function(x) {
-            var res = self.run(x)
-            return { 
-                value: res.value,
-                template: append(res.template, f(res.value) ? interpolate(template).run(x).template :  mnull())
+        return new tmpl(function(c){ 
+            var run = self.run(c)
+            var runT = interpolate(template).run(c)
+            return function(x) {
+                var res = run(x)
+                return { 
+                    value: res.value,
+                    template: append(res.template, f(res.value) ? runT(x).template :  mnull())
+                }
             }
         })
     }
@@ -73,16 +82,20 @@ function tmpl(run){
     }
     this.mapEnv = function(trans) {
         var self = this
-        return new tmpl(function(x) {
-            return self.run(trans(x))
-        })
+        return new tmpl(function (c) { 
+            var run = self.run(c)
+            return function(x) {
+                return run(trans(x))
+            }})
     }
     this.map = function(f) {
         var self = this
-        return new tmpl(function(r) {
-            var res = self.run(r)
-            return { value: f(res.value), template: res.template }
-        })
+        return new tmpl(function(c) { 
+            var run = self.run(c)
+            return function(r) {
+                var res = run(r)
+                return { value: f(res.value), template: res.template }
+            }})
     }
     this.and = function(other) {
         return this.flatMap(function(_) { return other } )
@@ -90,7 +103,13 @@ function tmpl(run){
     this.tell = function() {
         return this.flatMap(tell)
     }
-    this.bind = function() { return this }
+    this.withContext = function(name, value) { 
+        var self = this;
+        return new tmpl(function(c) { 
+            console.log(c.concat([{name: name, value: value}]))
+            return self.run(c.concat([{name: name, value: value}]))
+        })
+    }
 }
 
 repeat = function(template) {
@@ -110,47 +129,57 @@ var makeTag = function(tagName) {
     }
     
     var arg0 = arguments[0];
-    return new tmpl(function(x) {
-        var attrs = ""
-        for (var i in arg0) {
-            console.log(interpolate(arg0[i]).run(x).template)
-            attrs += i + '="'+ interpolate(arg0[i]).run(x).template + '"'
+    return new tmpl(function(c) { 
+        for (var i in arg0)
+            arg0[i] = interpolate(arg0[i]).run(c)
+        var runContent = content.run(c)
+        return function(x) {
+            var attrs = ""
+            for (var i in arg0) {
+                attrs += i + '="'+ arg0[i](x).template + '"'
+            }
+            var c = runContent(x)
+            return { 
+                value: content.value, 
+                template: '<' + tagName + ' ' + attrs + '>' + c.template + '</' + tagName + '>'
+            }
         }
-        var c = content.run(x)
-        return { value: content.value, template: '<' + tagName + ' ' + attrs + '>' + c.template + '</' + tagName + '>'}
     })
   }
 }
 
 function interpolate(str) {
     var parts = []
-    while (str) {
-        var i = str.indexOf('{{')
-        if (i < 0) {
-            parts.push(str)
-            break
+    return new tmpl(function(c) { 
+          while (str) {
+          var i = str.indexOf('{{')
+          if (i < 0) {
+              parts.push(str)
+              break
+          }
+          parts.push(str.slice(0, i))
+          str = str.slice(i + 2)
+          var j = str.indexOf('}}')
+          if (j < 0)
+              throw new Error('Parse error in template: Missing "}}"')
+          var expression = str.slice(0, j)
+          
+          parts.push(new Function(c.map(function(x){return x.name}), 'return ' + str.slice(0, j)))
+          str = str.slice(j + 2)
         }
-        parts.push(str.slice(0, i))
-        str = str.slice(i + 2)
-        var j = str.indexOf('}}')
-        if (j < 0)
-            throw new Error('Parse error in template: Missing }}')
-        var expression = str.slice(0, j)
-        parts.push(new Function('return ' + str.slice(0, j)))
-        str = str.slice(j + 2)
-    }
-    return new tmpl(function(x) {
-        var s = ''
-        var retVal = null
-        for (var i in parts) {
-            if (typeof parts[i] === 'string')
-                s += parts[i]
-            else {
-                retVal = parts[i].bind(x)()
-                s += retVal
+        return function(x) {
+            var s = ''
+            var retVal = null
+            for (var i in parts) {
+                if (typeof parts[i] === 'string')
+                    s += parts[i]
+                else {
+                    retVal = parts[i].bind(x).apply(null, c.map(function(x){return x.value}))
+                    s += retVal
+                }
             }
+            return { value: retVal, template: s }
         }
-        return { value: retVal, template: s }
     })
 }
 
@@ -159,8 +188,6 @@ span  = makeTag('span')
 table = makeTag('table')
 tr    = makeTag('tr')
 td    = makeTag('td')
-
-function bind(x, y, z) { return z }
 
 var test =
   div({id: 'container'},
@@ -180,8 +207,9 @@ tableTemplate =
              repeat(
               td({'class': '{{name}}-cell'},
                template))))
-           .bind(name, 'name')
+           .withContext('name', name)
 }
+
 var person = div('My name is {{this.name}}')
 personTable = tableTemplate('person', person)
 
